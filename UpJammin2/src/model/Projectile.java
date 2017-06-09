3a574c9a5207c5c3a2a0bf70d00705d7bd8c343a
Hazelsnap <Hazelsnap@10.20.121.76>
2017-06-09 15:56:29 +0100

    Unfinished Projectile class

---
+++ UpJammin2/src/model/Projectile.java
@@ -0,0 +1,42 @@
+package model;
+
+import java.awt.Graphics2D;
+import java.awt.geom.Point2D;
+
+public class Projectile extends Entity {
+
+	private Map map;
+	private int health;
+	private Point2D point;
+	private Point2D target;
+	private double distance;
+	private double pixelsPerTick;
+	private int speed;
+	
+	public Projectile(Map map, int health, Point2D point, Point2D target, int speed) {
+		super(map, health, point);
+		this.map = map;
+		this.health = health;
+		this.point = point;
+		this.target = target;
+		this.speed = speed;
+	}
+
+	@Override
+	public void tick() {
+		
+	}
+	
+	public void calculate() {
+		this.distance = point.distance(target);
+		pixelsPerTick = (distance/speed);
+		
+	}
+	
+	@Override
+	public void render(Graphics2D g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
