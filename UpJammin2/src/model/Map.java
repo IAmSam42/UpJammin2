13b92bb3ad932b1ac6e313a261dae9b870b5e517
Sam Holdcroft <sam.holdcroft42@gmail.com>
2017-06-09 15:00:01 +0100

    Implemented the Map class

--- UpJammin2/src/model/Map.java
+++ UpJammin2/src/model/Map.java
@@ -1,6 +1,93 @@
 package model;
 
+/**
+ * Contains information about the dimensions of the map, and which tiles 
+ * are being blocked by walls and turrets
+ * @author sam
+ *
+ */
 public class Map
 {
+	private int height;
+	private int width;
+	private int scale;
+	private boolean[][] blocked;
+	
+	/**
+	 * Constructor for a map object.
+	 * @param height The height of the map in grid references (not pixels).
+	 * @param width The width of the map in grid references (not pixels).
+	 * @param scale How many pixels per grid point.
+	 */
+	public Map(int height, int width, int scale)
+	{
+		this.setHeight(height);
+		this.setWidth(width);
+		this.setScale(scale);
+		
+		this.blocked = new boolean[width][height];
+	}
 
+	/**
+	 * Get the height of the map.
+	 * @return The height of the map.
+	 */
+	public int getHeight() 
+	{
+		return height;
+	}
+
+	/**
+	 * Set the height of the map.
+	 * @param height The new height of the map.
+	 */
+	public void setHeight(int height) 
+	{
+		this.height = height;
+	}
+
+	/**
+	 * Set the width of the map.
+	 * @return The width of the map.
+	 */
+	public int getWidth() 
+	{
+		return width;
+	}
+
+	/**
+	 * Set the width of the map.
+	 * @param width The new width of the map.
+	 */
+	public void setWidth(int width)
+	{
+		this.width = width;
+	}
+
+	/**
+	 * Get the scale of the map.
+	 * @return The scale of the map.
+	 */
+	public int getScale() 
+	{
+		return scale;
+	}
+
+	/**
+	 * Set the scale of the map.
+	 * @param scale The new scale of the map.
+	 */
+	public void setScale(int scale) 
+	{
+		this.scale = scale;
+	}
+
+	/**
+	 * Get the array of blocked grid points.
+	 * @return The blocked grid points.
+	 */
+	public boolean[][] getBlocked() 
+	{
+		return blocked;
+	}
 }
