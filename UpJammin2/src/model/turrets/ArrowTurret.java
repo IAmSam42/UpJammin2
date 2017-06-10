41a25b01c279bde91e5cd8ba9937a87872827965
Mitch <Mitch@DESKTOP-IOIBF79>
2017-06-10 18:46:42 +0100

    added more images to the types of turrets

---
+++ UpJammin2/src/model/turrets/ArrowTurret.java
@@ -0,0 +1,24 @@
+package model.turrets;
+
+import java.awt.Graphics2D;
+import java.awt.Point;
+
+import javax.swing.ImageIcon;
+
+import model.Map;
+import model.Turret;
+
+public class ArrowTurret extends Turret 
+{
+
+	public ArrowTurret(Map map, int health, Point location, int range, int damage, int fireRate, int fireSpeed) 
+	{
+		super(map, health, location, range, damage, fireRate, fireSpeed);
+		// TODO Auto-generated constructor stub
+	}
+	public void render(Graphics2D g) 
+	{
+
+	}
+
+}
---
+++ UpJammin2/src/model/turrets/CannonTurret.java
@@ -0,0 +1,30 @@
+package model.turrets;
+
+import java.awt.Graphics2D;
+import java.awt.Point;
+
+import javax.swing.ImageIcon;
+
+import model.Map;
+import model.Turret;
+
+public class CannonTurret extends Turret {
+
+	public CannonTurret(Map map, int health, Point location, int range, int damage, int fireRate, int fireSpeed) {
+		super(map, health, location, range, damage, fireRate, fireSpeed);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void render(Graphics2D g) 
+	{
+		if(this.isFacingRight())
+		{
+			g.drawImage(new ImageIcon("resources/cannonRight.jpg").getImage(), this.getMap().toGridPoint((this.getPoint())).x, this.getMap().toGridPoint((this.getPoint())).y, null);
+		}
+		else
+		{
+			g.drawImage(new ImageIcon("resources/cannonLeft.jpg").getImage(), this.getMap().toGridPoint((this.getPoint())).x, this.getMap().toGridPoint((this.getPoint())).y, null);
+		}
+	}
+
+}
