6e8e5e079956b1de2636cc91219d1a48704ba524
SamDurdy <samdurdy@hotmail.co.uk>
2017-06-10 17:11:28 +0100

    Did some codes

--- UpJammin2/src/engine/Config.java
+++ UpJammin2/src/engine/Config.java
@@ -1,6 +1,7 @@
 package engine;
 
 public class Config {
-	public static String upgrade_file = "resources/upgrades.json";
+	public static String upgrade_file = "resources/financeUpgrades.json";
 	public static String levels_file = "resources/levels.json";
+	
 }
