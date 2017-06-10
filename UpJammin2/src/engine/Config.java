f8c01454bf8dea65fe22c1f12eb6b9714f1c6a9f
Victor R. Cardoso <labrax@users.noreply.github.com>
2017-06-10 17:21:25 +0100

    Merge branch 'master' of https://github.com/IAmSam42/UpJammin2

--- UpJammin2/src/engine/Config.java
+++ UpJammin2/src/engine/Config.java
@@ -2,7 +2,7 @@ package engine;
 
 public class Config {
 	//jsons
-	public static String upgrade_file = "resources/upgrades.json";
+	public static String upgrade_file = "resources/financeUpgrades.json";
 	public static String levels_file = "resources/levels.json";
 	
 	//imgs
