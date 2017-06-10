8194a88863eb357dce2e4982bdabcfd94ca53987
Mitch <Mitch@DESKTOP-IOIBF79>
2017-06-10 18:50:21 +0100

    fixed graphics g bug

--- UpJammin2/src/model/enemies/BowlerAlpaca.java
+++ UpJammin2/src/model/enemies/BowlerAlpaca.java
@@ -1,6 +1,7 @@
 package model.enemies;
 
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Point;
 
 import javax.swing.ImageIcon;
@@ -8,19 +9,16 @@ import javax.swing.ImageIcon;
 import model.Enemy;
 import model.Map;
 
-public class BowlerAlpaca extends Enemy {
+public class BowlerAlpaca extends Enemy{
 
 	public BowlerAlpaca(Map map, int health, Point point) {
 		super(map, health, point);
 		// TODO Auto-generated constructor stub
-
 	}
 
 	@Override
-	public void render(Graphics g) {
-//		System.out.println("render " + this.getMap().toGridPoint((this.getPoint())).x + " " +  this.getMap().toGridPoint((this.getPoint())).y);
-//		System.out.println("COord " + this.getPoint());
-		g.drawImage(new ImageIcon("resources/EnemyAlpacaBowlerHatRight.png").getImage(),this.getMap().toGridPoint((this.getPoint())).x*this.getMap().getScale(), this.getMap().toGridPoint((this.getPoint())).y*this.getMap().getScale(), null);
-
+	public void render(Graphics g) 
+	{
+		g.drawImage(new ImageIcon("resources/EnemyAlpacaBowlerHatRight.jpg").getImage(), this.getMap().toGridPoint((this.getPoint())).x, this.getMap().toGridPoint((this.getPoint())).y, null);
 	}
 }
