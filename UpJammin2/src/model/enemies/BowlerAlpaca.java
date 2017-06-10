b6781760900ad6ff7a358e7c357f0fc59f15ca13
Sam Holdcroft <sam.holdcroft42@gmail.com>
2017-06-10 18:08:02 +0100

    Removeal of prints

--- UpJammin2/src/model/enemies/BowlerAlpaca.java
+++ UpJammin2/src/model/enemies/BowlerAlpaca.java
@@ -19,8 +19,8 @@ public class BowlerAlpaca extends Enemy {
 
 	@Override
 	public void render(Graphics g) {
-		System.out.println("render " + this.getMap().toGridPoint((this.getPoint())).x + " " +  this.getMap().toGridPoint((this.getPoint())).y);
-		System.out.println("COord " + this.getPoint());
+//		System.out.println("render " + this.getMap().toGridPoint((this.getPoint())).x + " " +  this.getMap().toGridPoint((this.getPoint())).y);
+//		System.out.println("COord " + this.getPoint());
 		g.drawImage(new ImageIcon("resources/EnemyAlpacaBowlerHatRight.png").getImage(),this.getMap().toGridPoint((this.getPoint())).x*this.getMap().getScale(), this.getMap().toGridPoint((this.getPoint())).y*this.getMap().getScale(), null);
 
 	}
