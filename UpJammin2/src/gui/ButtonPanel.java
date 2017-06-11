09613365cd2c4b909cd8167de5bce84300c415f2
Mitch <Mitch@DESKTOP-IOIBF79>
2017-06-11 10:59:56 +0100

    added a wizard tower

--- UpJammin2/src/gui/ButtonPanel.java
+++ UpJammin2/src/gui/ButtonPanel.java
@@ -18,7 +18,7 @@ import model.Map.blockType;
 
 public class ButtonPanel extends JPanel implements Observer {
 	public enum Selected {
-		None, ArrowTurret, CannonTurret, Wall
+		None, ArrowTurret, CannonTurret, Wall ,Wizard
 	}
 	
 	private Bank bank;
@@ -26,10 +26,12 @@ public class ButtonPanel extends JPanel implements Observer {
 	private String arrowLabel;
 	private String cannonLabel;
 	private String wallLabel;
+	private String wizardLabel;
 	
 	private JToggleButton arrowTurret;
 	private JToggleButton cannonTurret;
 	private JToggleButton wall;
+	private JToggleButton wizard;
 	
 	public ButtonPanel(Bank bank) {
 		
@@ -38,12 +40,12 @@ public class ButtonPanel extends JPanel implements Observer {
 		this.arrowLabel = "Arrow Turret - " + bank.getCost(blockType.ArrowTurret);
 		this.cannonLabel = "Cannon Turret - " + bank.getCost(blockType.CannonTurret);
 		this.wallLabel = "Wall - " + bank.getCost(blockType.Wall);
-		
+		this.wizardLabel = "Wizard - " + "";
 		bank.addObserver(this);
 		arrowTurret = new JToggleButton(arrowLabel);
 		cannonTurret = new JToggleButton(cannonLabel);
 		wall = new JToggleButton(wallLabel);
-	
+		wizard = new JToggleButton(wizardLabel);
 		
 		JPanel buttonPanel = new JPanel();
 		//if not set to tower, set to tower, otherwise set to none
@@ -52,6 +54,7 @@ public class ButtonPanel extends JPanel implements Observer {
 			public void actionPerformed(ActionEvent arg0) {
 					if(button != Selected.ArrowTurret) {
 						button = Selected.ArrowTurret;
+						wizard.setSelected(false);
 						arrowTurret.setSelected(true);
 						wall.setSelected(false);
 						cannonTurret.setSelected(false);
@@ -69,6 +72,7 @@ public class ButtonPanel extends JPanel implements Observer {
 					if(button != Selected.CannonTurret) {
 						button = Selected.CannonTurret;
 						cannonTurret.setSelected(true);
+						wizard.setSelected(false);
 						wall.setSelected(false);
 						arrowTurret.setSelected(false);
 					}
@@ -87,6 +91,7 @@ public class ButtonPanel extends JPanel implements Observer {
 				if(button != Selected.Wall) {
 					button = Selected.Wall;
 					wall.setSelected(true);
+					wizard.setSelected(false);
 					arrowTurret.setSelected(false);
 					cannonTurret.setSelected(false);
 				}
@@ -96,11 +101,27 @@ public class ButtonPanel extends JPanel implements Observer {
 				}
 			}
 		});
+		wizard.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(button != Selected.Wizard) {
+					button = Selected.Wizard;
+					wizard.setSelected(true);
+					arrowTurret.setSelected(false);
+					cannonTurret.setSelected(false);
+					wall.setSelected(false);
+			}
+			else {
+				button = Selected.None;
+				wizard.setSelected(false);
+			}
+		}
+	});
 		
 		buttonPanel.setLayout(new GridLayout(1,3));
 		buttonPanel.add(arrowTurret);
 		buttonPanel.add(cannonTurret);
 		buttonPanel.add(wall);
+		buttonPanel.add(wizard);
 		
 		add(buttonPanel,BorderLayout.CENTER);
 		
@@ -115,6 +136,7 @@ public class ButtonPanel extends JPanel implements Observer {
 		arrowTurret.setText("Arrow Turret - " + bank.getCost(blockType.ArrowTurret));
 		cannonTurret.setText("Cannon Turret - " + bank.getCost(blockType.CannonTurret));
 		wall.setText("Wall - " + bank.getCost(blockType.Wall));
+		wizard.setText("Wizard - " + bank.getCost(blockType.Wall));
 		
 	}
 }
